443e7f786027ca59b6c5b6a593d1f8c309579589
Apeksha <apeksha.singh287@gmail.com>
2026-09-24 21:31:46 +0530

    Updated circle program to display circumference

--- circle.py
+++ circle.py
@@ -1,4 +1,7 @@
 import math
 radius = float(input("Enter the radius of the circle: "))
 area = math.pi * radius * radius
+circumference = 2 * math.pi * radius
+
 print("Area of the circle =", area)
+print("Circumference of the circle =", circumference)
